0034c66327e0b5ebf23971a8a6eab43da47daaf3
LucasFerreiraUBA <lferreira@fi.uba.ar>
2022-11-04 02:58:34 -0300

    Terminar de implementar funcion de redimensión e iterador interno

--- diccionario_test.go
+++ diccionario_test.go
@@ -17,7 +17,6 @@ func TestDiccionarioVacio(t *testing.T) {
 	require.False(t, dic.Pertenece("A"))
 	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Obtener("A") })
 	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Borrar("A") })
-
 }
 
 func TestUnElement(t *testing.T) {
@@ -54,24 +53,22 @@ func TestDiccionarioGuardar(t *testing.T) {
 
 	require.False(t, dic.Pertenece(claves[1]))
 	require.False(t, dic.Pertenece(claves[2]))
-	/*
-		dic.Guardar(claves[1], valores[1])
-			require.True(t, dic.Pertenece(claves[0]))
-				require.True(t, dic.Pertenece(claves[1]))
-				require.EqualValues(t, 2, dic.Cantidad())
-				require.EqualValues(t, valores[0], dic.Obtener(claves[0]))
-				require.EqualValues(t, valores[1], dic.Obtener(claves[1]))
+	dic.Guardar(claves[1], valores[1])
+	require.True(t, dic.Pertenece(claves[0]))
+	require.True(t, dic.Pertenece(claves[1]))
+	require.EqualValues(t, 2, dic.Cantidad())
+	require.EqualValues(t, valores[0], dic.Obtener(claves[0]))
+	require.EqualValues(t, valores[1], dic.Obtener(claves[1]))
 
-				require.False(t, dic.Pertenece(claves[2]))
-				dic.Guardar(claves[2], valores[2])
-				require.True(t, dic.Pertenece(claves[0]))
-				require.True(t, dic.Pertenece(claves[1]))
-				require.True(t, dic.Pertenece(claves[2]))
-				require.EqualValues(t, 3, dic.Cantidad())
-				require.EqualValues(t, valores[0], dic.Obtener(claves[0]))
-				require.EqualValues(t, valores[1], dic.Obtener(claves[1]))
-				require.EqualValues(t, valores[2], dic.Obtener(claves[2]))
-	*/
+	require.False(t, dic.Pertenece(claves[2]))
+	dic.Guardar(claves[2], valores[2])
+	require.True(t, dic.Pertenece(claves[0]))
+	require.True(t, dic.Pertenece(claves[1]))
+	require.True(t, dic.Pertenece(claves[2]))
+	require.EqualValues(t, 3, dic.Cantidad())
+	require.EqualValues(t, valores[0], dic.Obtener(claves[0]))
+	require.EqualValues(t, valores[1], dic.Obtener(claves[1]))
+	require.EqualValues(t, valores[2], dic.Obtener(claves[2]))
 }
 
 func TestReemplazoDato(t *testing.T) {
@@ -207,9 +204,8 @@ func TestClaveVacia(t *testing.T) {
 	t.Log("Guardamos una clave vacía (i.e. \"\") y deberia funcionar sin problemas")
 	dic := TDADiccionario.CrearHash[string, string]()
 	clave := ""
-	require.False(t, dic.Pertenece(clave))
 	dic.Guardar(clave, clave)
-	require.True(t, dic.Pertenece(clave), "DEBE SER TRUE")
+	require.True(t, dic.Pertenece(clave))
 	require.EqualValues(t, 1, dic.Cantidad())
 	require.EqualValues(t, clave, dic.Obtener(clave))
 }
@@ -428,28 +424,21 @@ func TestIteradorNoLlegaAlFinal(t *testing.T) {
 	dic.Guardar(claves[0], "")
 	dic.Guardar(claves[1], "")
 	dic.Guardar(claves[2], "")
+
 	dic.Iterador()
-	/*
-		iter2 := dic.Iterador()
-		iter2.Siguiente()
-	*/
+	iter2 := dic.Iterador()
+	iter2.Siguiente()
 	iter3 := dic.Iterador()
-	clave, _ := iter3.VerActual()
-	require.EqualValues(t, clave, "B")
 	primero := iter3.Siguiente()
-	require.EqualValues(t, primero, "B")
 	segundo := iter3.Siguiente()
-	require.EqualValues(t, segundo, "C")
 	tercero := iter3.Siguiente()
-	require.False(t, iter3.HaySiguiente(), "NO HAY MAS PARA ITERAR")
+	require.False(t, iter3.HaySiguiente())
 	require.NotEqualValues(t, primero, segundo)
 	require.NotEqualValues(t, tercero, segundo)
-	/*
-	   require.NotEqualValues(t, primero, tercero)
-	   require.NotEqualValues(t, -1, buscar(primero, claves))
-	   require.NotEqualValues(t, -1, buscar(segundo, claves))
-	   require.NotEqualValues(t, -1, buscar(tercero, claves))
-	*/
+	require.NotEqualValues(t, primero, tercero)
+	require.NotEqualValues(t, -1, buscar(primero, claves))
+	require.NotEqualValues(t, -1, buscar(segundo, claves))
+	require.NotEqualValues(t, -1, buscar(tercero, claves))
 }
 
 func TestPruebaIterarTrasBorrados(t *testing.T) {
